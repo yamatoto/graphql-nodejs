0db99af008d3fbbd15f983bec28ea0e0769a6161
yamatoto <mftb35ymt@yahoo.co.jp>
2021-06-05 11:44:56 +0900

    impl return rootValue

---
+++ src/server.ts
@@ -0,0 +1,28 @@
+import express from "express";
+const { graphqlHTTP } = require("express-graphql");
+import { buildSchema } from "graphql";
+
+const schema = buildSchema(`
+  type Query {
+    message: String
+  }
+`);
+
+const root = {
+  message: () => "Hello World",
+};
+
+const app = express();
+
+app.use(
+  "/graphql",
+  graphqlHTTP({
+    schema,
+    rootValue: root,
+    graphiql: true,
+  })
+);
+
+app.listen(4000, () =>
+  console.log("Express GraphQL Server Now Running On localhost:4000/graphql")
+);
